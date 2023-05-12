1692dca10da85ce7f285d89e4a4d153e69b76ef6
HimanshuDev <himandevofficial@gmail.com>
2023-05-12 11:27:51 +0530

    done

---
+++ android/app/src/main/kotlin/com/example/flutterminitopics/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterminitopics
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/expandedlayout.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class ExampleExpandedLayout extends StatelessWidget {
+
+ @override
+ Widget build(BuildContext context) {
+  return Scaffold(
+   body:Column(
+    children: [
+     Expanded(
+      child:Row(
+       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+       children: [
+        Expanded(
+         child:ElevatedButton(
+          onPressed: () {
+
+          }, child: Text("Stack"),
+         )
+        ),
+        Expanded(
+            child:ElevatedButton(
+             onPressed: () {
+
+             }, child: Text("Forms"),
+            )
+        )
+       ],
+      )
+     ),
+
+     Expanded(
+      child: Center(
+       child:ElevatedButton(
+        onPressed: (){
+
+        },
+        child: Text("Button 1")
+       )
+      ),
+     ),
+
+     Expanded(
+      child: Align(
+       alignment: Alignment.bottomRight,
+       child: ElevatedButton(
+        onPressed: (){
+
+        },
+        child:Text("Button 2")
+       ),
+      ),
+     )
+
+    ],
+   )
+  );
+ }
+
+}
